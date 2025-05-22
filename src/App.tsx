2000c8ef0094d51bbc000e3b2358f7900ea69e58
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-05-22 15:04:41 -0400

    Update App.tsx to add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import Welcome from './Popups/Welcome'
 import { Route, Routes } from 'react-router-dom'
 import JoinSession from './Popups/JoinSession'
-import CreateSession from './Popups/CreateSession'
+import CreateSession from './Popups/SessionCreated'
+import BridgeToCreateSession from './Popups/BridgeToCreateSession'
 
 function App() {
   return (
       <Routes>
         <Route index element={<Welcome />}/>
-        <Route path='/join' element={<JoinSession />}/>
-        <Route path='/create' element={<CreateSession />} />
+        <Route path='/join-session' element={<JoinSession />}/>
+        <Route path='/create-session' element ={<BridgeToCreateSession/>} />
+        <Route path='/created' element={<CreateSession />} />
       </Routes>
   )
 }
